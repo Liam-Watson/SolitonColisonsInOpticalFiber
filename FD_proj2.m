fe2cc364c810fdc20473284833cc4143bddef719
Liam Watson <liam@watson.org.za>
2021-06-12 12:40:46 +0200

    First report draft, testing code for both numerical schemes

--- FD_proj2.m
+++ FD_proj2.m
@@ -1,19 +1,25 @@
 clear all; %Ensure testing is not effected by past computation
 clc;
+tiledlayout(2,2)
+figCount = 0;
 
+for v = [0.5, 1, 2 , 10]
+    figCount = figCount + 1;
 L = 100; %Overall simulation interval
-N = 556; %Number of space subdivisions
+N = 2000; %Number of space subdivisions
 h = L/N; %Size of mesh spacing
 x = [0:h:(L-h)]'; %Space mesh
 tau = h^2/3; %Time step
 
 A1 = 1; %Inital amplitude of soliton 1
 A2 = 1; %Intial amplitude of soliton 2
-S = 0.5; %Nonlinarity saturation
+S = 0.0; %Nonlinarity saturation
 xPos1 = 30; %Starting position of wave peak in x direction
 xPos2 = 60; %Starting position of wave peak in x direction
-v1 = 1; %Velocity of soliton 1
-v2 = -1; %%Velocity of soliton 2
+v1 = v;
+v2 = -v;
+%v1 = 1; %Velocity of soliton 1
+%v2 = -1; %%Velocity of soliton 2
 psi = A1*sech(A1*(x-xPos1)).*exp(1i*v1*(x-xPos1))...%Soliton 1 Inital
       + A2*sech(A2*(x-xPos2)).*exp(1i*v2*(x-xPos2));%Soliton 2 Inital
 
@@ -27,8 +33,9 @@ A = diag((1i-s)*ones(N,1),0)... %Set the FD matrix values on diagonals
     +diag(s2*ones(N-1,1),-1);
 A(1,N) = s2; %Set periodic BC elements in matrix A
 A(N,1) = s2;
+A = inv(A);
 rhs = zeros(size(x)); %Create RHS vector
-time = 1000; %Max time
+time = 6000/v; %Max time
 
 X = [x]; %Used for 3D plot
 Y = [abs(psi)];%Used for 3D plot
@@ -51,13 +58,17 @@ for t=1:time %Loop until max time reached
             -(2*tau*abs(psi(N)).^2.*psi(N))/(1+S*sin(abs(psi(N)).^2));
             %-(tau*abs(psi(N-1)).^2.*psi(N-1))/(1+S*sin(abs(psi(N-1))))...
             %-(tau*abs(psi(1)).^2.*psi(1))/(1+S*sin(abs(psi(1))));
-            
-    psi=A\rhs; %Solve for psi
+    %psi = A\rhs;
+    psi= A*rhs; %Solve for psi
     Y = [abs(psi) Y]; %%Used for 3D plot
-    Const = trapz(x,abs(psi).^2) %The conserved quantity
+    Const = trapz(x,abs(psi).^2); %The conserved quantity
     
-    plot(x,abs(psi)), drawnow %Plot 2D updated solution for time t
+    %plot(x,abs(psi)), drawnow %Plot 2D updated solution for time t
 end
-
+%subplot(2,2,figCount);
+figure(figCount);
 h = surf(X,Z,Y') %Plot 3D, x, t, psi
 set(h,'LineStyle','none')
+title("|v|=" + v + ",   D=" + compose("%9.7f",Const))
+pause;
+end
--- split_step_proj2.m
+++ split_step_proj2.m
@@ -1,23 +1,32 @@
 clear all; %Ensure testing is not effected by past computation
 clc;
+tiledlayout(2,2)
+figCount = 0;
 
+%for xx = [5,10,30, 40]
+%    figCount = figCount + 1;
 L = 100; %Simulation space interval
-N = 1556; %Number of mesh points
+N = 2000; %Number of mesh points
 h = L/N; %Size of mesh spacing
 x = 0:h:(L-h); %Discretized spacial interval
-tau = h^2/5;%The method is explicit so there is a stability condition
+%tau = h^2/3;%The method is explicit so there is a stability condition
+tau = 8.333333333333335e-04;
 A1 = 1; %Aplitude of soliton 1
 A2 = 1; %Aplitude of soliton 2
 S = 0.5; %Saturation of nonlinearity
+%xPos1 = 50 - xx;
+%xPos2 = 50 + xx;
 xPos1 = 30;  %Starting position of wave peak in x direction
 xPos2 = 60; %Starting position of wave peak in x direction
+%v1 = v;
+%v2 = -v;
 v1 = 5;  %Velocity of soliton 1
 v2 = -5; %Velocity of soliton 1
 psi = A1*sech(A1*(x-xPos1)).*exp(1i*v1*(x-xPos1))...%Soliton 1 Inital
       + A2*sech(A2*(x-xPos2)).*exp(1i*v2*(x-xPos2));%Soliton 2 Inital
 %plot(x,abs(psi))%Plot inital configuration. Removed for testing speed
-time=2500; %Max time
-
+%time=200*xx; %Max time
+time = 6000;
 %Used for 3D plot
 X = [x];
 Y = [abs(psi)];
@@ -27,12 +36,17 @@ vec2 = 4*pi^2/L^2*[0:N/2 - 1 -N/2:-1].^2;
 for ti=1:time
     psi = psi.*exp((tau*2.i*abs(psi).^2)./(1+S*sin(abs(psi).^2))); %Update psi
     psi = ifft(fft(psi).*exp(-1i*tau*vec2)); %Update psi with FFT and IFFT approxmiation
-    Const = trapz(x,abs(psi).^2) %The conserved quantity
+    Const = trapz(x,abs(psi).^2); %The conserved quantity
     %X = [X;x];
     Y = [Y;abs(psi)]; %Used for 3D plot
     
-    plot(x,abs(psi)), drawnow %Plot 2D updated solution for time t
+    %plot(x,abs(psi)), drawnow %Plot 2D updated solution for time t
 end
-
+%nexttitle
+%subplot(2,2,figCount);
+%[X,Z,Y] = reducem(X,Z,Y,0.000001);
 h = surf(X,Z,Y) %Plot 3D, x, t, psi
 set(h,'LineStyle','none')
+%Const = round(Const,6,'significant');
+%title("|x1 - x2|=" + 2*xx + ",   D=" + compose("%9.7f",Const))
+%end
